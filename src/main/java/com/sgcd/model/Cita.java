47b0306264bfc9e4e9333e73779abbbae3464653
Camba <fercambafcm@gmail.com>
2024-09-04 19:46:17 -0600

    update

--- src/main/java/com/sgcd/model/Cita.java
+++ src/main/java/com/sgcd/model/Cita.java
@@ -1,6 +1,12 @@
 package com.sgcd.model;
 
+import com.sgcd.util.DatabaseConfig;
+
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Time;
 
 public class Cita {
@@ -25,4 +31,66 @@ public class Cita {
 
     public Time getHora() { return hora; }
     public void setHora(Time hora) { this.hora = hora; }
+
+    // Metodo de creacion
+    public void create(Cita cita) throws SQLException {
+        String sql = "INSERT INTO Citas (paciente_id, medico_id, fecha, hora) VALUES (?,?,?,?)";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, cita.getPacienteId());
+            stmt.setInt(2, cita.getMedicoId());
+            stmt.setDate(3, cita.getFecha());
+            stmt.setTime(4, cita.getHora());
+            stmt.executeUpdate();
+        }
+    }
+
+    // Metodo para buscar una cita por ID
+    public Cita findById(int id) throws SQLException {
+        String sql = "SELECT * FROM Citas WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+        PreparedStatement stmt = con.prepareStatement(sql)){
+
+             stmt.setInt(1, id);
+             try (ResultSet rs = stmt.executeQuery()){
+                 if (rs.next()){
+                     Cita cita = new Cita();
+                     cita.setId(rs.getInt("id"));
+                     cita.setPacienteId(rs.getInt("paciente_id"));
+                     cita.setMedicoId(rs.getInt("medico_id"));
+                     cita.setFecha(rs.getDate("fecha"));
+                     cita.setHora(rs.getTime("hora"));
+                     return cita;
+                 }
+             }
+        }
+        return null;
+    }
+
+    // Metodo para editar
+    public void update(Cita cita) throws SQLException {
+        String sql = "UPDATE Citas SET paciente_id = ?, medico_id = ?, fecha = ?, hora = ? WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)){
+
+            stmt.setInt(1, cita.getPacienteId());
+            stmt.setInt(2, cita.getMedicoId());
+            stmt.setDate(3, cita.getFecha());
+            stmt.setTime(4, cita.getHora());
+            stmt.setInt(5, cita.getId());
+            stmt.executeUpdate();
+        }
+    }
+
+    // Metodo para eliminar
+    public void delete(Cita cita) throws SQLException {
+        String sql = "DELETE FROM Citas WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)){
+
+            stmt.setInt(1, cita.getId());
+            stmt.executeUpdate();
+        }
+    }
 }
--- src/main/java/com/sgcd/model/Consulta.java
+++ src/main/java/com/sgcd/model/Consulta.java
@@ -1,7 +1,8 @@
 package com.sgcd.model;
 
-import java.sql.Date;
-import java.sql.Time;
+import com.sgcd.util.DatabaseConfig;
+
+import java.sql.*;
 
 public class Consulta {
     private int id;
@@ -25,4 +26,65 @@ public class Consulta {
 
     public Time getHora() { return hora; }
     public void setHora(Time hora) { this.hora = hora; }
+
+    // Metodo de creacion
+    public void create(Consulta consulta) throws SQLException {
+        String sql = "INSERT INTO Consulta (paciente_id, medico_id, fecha, hora) VALUES (?,?,?,?)";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setInt(1, consulta.getPacienteId());
+            stmt.setInt(2, consulta.getMedicoId());
+            stmt.setDate(3, consulta.getFecha());
+            stmt.setTime(4, consulta.getHora());
+            stmt.executeUpdate();
+        }
+    }
+
+    // Metodo para buscar una cita por ID
+    public Consulta findById(int id) throws SQLException {
+        String sql = "SELECT * FROM Consulta WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)){
+
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()){
+                if (rs.next()){
+                    Consulta consulta = new Consulta();
+                    consulta.setId(rs.getInt("id"));
+                    consulta.setPacienteId(rs.getInt("paciente_id"));
+                    consulta.setMedicoId(rs.getInt("medico_id"));
+                    consulta.setFecha(rs.getDate("fecha"));
+                    consulta.setHora(rs.getTime("hora"));
+                    return consulta;
+                }
+            }
+        }
+        return null;
+    }
+
+    // Metodo para editar
+    public void update(Consulta consulta) throws SQLException {
+        String sql = "UPDATE Consulta SET paciente_id = ?, medico_id = ?, fecha = ?, hora = ? WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)){
+
+            stmt.setInt(1, consulta.getPacienteId());
+            stmt.setInt(2, consulta.getMedicoId());
+            stmt.setDate(3, consulta.getFecha());
+            stmt.setTime(4, consulta.getHora());
+            stmt.setInt(5, consulta.getId());
+            stmt.executeUpdate();
+        }
+    }
+
+    public void delete(Consulta consulta) throws SQLException {
+        String sql = "DELETE FROM Consulta WHERE id = ?";
+        try (Connection con = DatabaseConfig.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)){
+
+            stmt.setInt(1, consulta.getId());
+            stmt.executeUpdate();
+        }
+    }
 }
