0ba204c5fc242a03504191f23dcefe1b56fb0336
Camba <fercambafcm@gmail.com>
2024-09-19 17:28:11 -0600

    sucursal y medico DAO

--- src/main/java/com/sgcd/dao/MedicoDAO.java
+++ src/main/java/com/sgcd/dao/MedicoDAO.java
@@ -13,20 +13,22 @@ import java.util.List;
 
 public class MedicoDAO {
 
-    // Método de creación
+    // Método de creación (actualizado para sucursal y email)
     public int create(Medico medico) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         int registros = 0;
-        String SQL_INSERT = "INSERT INTO medicos (usuario, contrasena, nombre, apellidos, especialidad) VALUES (?, ?, ?, ?, ?)";
+        String SQL_INSERT = "INSERT INTO medicos (usuario, contrasena, email, nombre, apellidos, idsucursal, especialidad) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             conn = getConnection();
             stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, medico.getUsuario());
             stmt.setString(2, medico.getContrasena());
-            stmt.setString(3, medico.getNombre());
-            stmt.setString(4, medico.getApellidos());
-            stmt.setString(5, medico.getEspecialidad());
+            stmt.setString(3, medico.getEmail());
+            stmt.setString(4, medico.getNombre());
+            stmt.setString(5, medico.getApellidos());
+            stmt.setString(6, medico.getIdsucursal());
+            stmt.setString(7, medico.getEspecialidad());
             registros = stmt.executeUpdate();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
@@ -37,7 +39,7 @@ public class MedicoDAO {
         return registros;
     }
 
-    // Metodo para traer todos los registros
+    // Metodo para traer todos los registros (actualizado para sucursal y email)
     public List<Medico> obtenerMedicos() throws SQLException {
         List<Medico> medicos = new ArrayList<>();
         Connection conn = null;
@@ -55,8 +57,10 @@ public class MedicoDAO {
                 medico.setId(rs.getInt("id"));
                 medico.setUsuario(rs.getString("usuario"));
                 medico.setContrasena(rs.getString("contrasena"));
+                medico.setEmail(rs.getString("email"));
                 medico.setNombre(rs.getString("nombre"));
                 medico.setApellidos(rs.getString("apellidos"));
+                medico.setIdsucursal(rs.getString("idsucursal"));
                 medico.setEspecialidad(rs.getString("especialidad"));
 
                 medicos.add(medico);
@@ -71,22 +75,24 @@ public class MedicoDAO {
         return medicos;
     }
 
-    // Metodo para editar
+    // Metodo para editar (actualizado para sucursal y email)
     public int actualizar(Medico medico) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         int registros = 0;
-        String SQL_UPDATE = "UPDATE medicos SET usuario = ?, contrasena = ?, nombre = ?, apellidos = ?, especialidad = ? WHERE id = ?";
+        String SQL_UPDATE = "UPDATE medicos SET usuario = ?, contrasena = ?, email = ?, nombre = ?, apellidos = ?, idsucursal = ?, especialidad = ? WHERE id = ?";
 
         try {
             conn = getConnection();
             stmt = conn.prepareStatement(SQL_UPDATE);
             stmt.setString(1, medico.getUsuario());
             stmt.setString(2, medico.getContrasena());
-            stmt.setString(3, medico.getNombre());
-            stmt.setString(4, medico.getApellidos());
-            stmt.setString(5, medico.getEspecialidad());
-            stmt.setInt(6, medico.getId());
+            stmt.setString(3, medico.getEmail());
+            stmt.setString(4, medico.getNombre());
+            stmt.setString(5, medico.getApellidos());
+            stmt.setString(6, medico.getIdsucursal());
+            stmt.setString(7, medico.getEspecialidad());
+            stmt.setInt(8, medico.getId());
             registros = stmt.executeUpdate();
             System.out.println(registros);
         } catch (SQLException ex) {
@@ -98,7 +104,7 @@ public class MedicoDAO {
         return registros;
     }
 
-    // Método para buscar un médico por ID
+    // Método para buscar un médico por ID (actualizado para sucursal y email)
     public Medico findById(int id) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -117,8 +123,10 @@ public class MedicoDAO {
                 medico.setId(rs.getInt("id"));
                 medico.setUsuario(rs.getString("usuario"));
                 medico.setContrasena(rs.getString("contrasena"));
+                medico.setEmail(rs.getString("email"));
                 medico.setNombre(rs.getString("nombre"));
                 medico.setApellidos(rs.getString("apellidos"));
+                medico.setIdsucursal(rs.getString("idsucursal"));
                 medico.setEspecialidad(rs.getString("especialidad"));
             }
         } catch (SQLException ex) {
@@ -131,7 +139,7 @@ public class MedicoDAO {
         return medico;
     }
 
-    // Método para eliminar
+    // Método para eliminar (actualizado para sucursal y email xd bueno este ni actualizarlo es necesario)
     public int delete(int id) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
@@ -152,5 +160,40 @@ public class MedicoDAO {
         return registros;
     }
 
+    // Metodo para buscar por idSucursal (actualizado para sucursal y email)
+    public List<Medico> obtenerMedicosPorSucursal(int Idsucursal) throws SQLException {
+        List<Medico> medicos = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM medicos WHERE idsucursal = ?";
+
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, Idsucursal);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Medico medico = new Medico();
+                medico.setId(rs.getInt("id"));
+                medico.setUsuario(rs.getString("usuario"));
+                medico.setContrasena(rs.getString("contrasena"));
+                medico.setEmail(rs.getString("email"));
+                medico.setNombre(rs.getString("nombre"));
+                medico.setApellidos(rs.getString("apellidos"));
+                medico.setIdsucursal(rs.getString("idsucursal"));
+                medico.setEspecialidad(rs.getString("especialidad"));
+                medicos.add(medico);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (rs != null) close(rs);
+            if (stmt != null) close(stmt);
+            if (conn != null) close(conn);
+        }
+        return medicos;
+    }
 
 }
