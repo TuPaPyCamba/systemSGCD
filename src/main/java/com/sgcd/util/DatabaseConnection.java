8bef59161a0e49afd0abf628bc85107e2a17037d
Acetilsalicilic <1Uncanalmas@gmail.com>
2024-09-19 16:38:05 -0600

    Solved conflicts with backend branch

--- src/main/java/com/sgcd/util/DatabaseConnection.java
+++ src/main/java/com/sgcd/util/DatabaseConnection.java
@@ -6,7 +6,8 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/sistema_dental";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "pass";
+
 
     public static Connection getConnection() throws SQLException {
         try {
